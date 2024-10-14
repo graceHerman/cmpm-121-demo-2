339139ee9a00ca06876cfc3abd83fb3fb49e47ca
graceHerman <151581400+graceHerman@users.noreply.github.com>
2024-10-14 11:54:52 -0700

    Started Completed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Hello DENO";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// File Path: '/Users/gracelilanhermangmail.com/Desktop/Fall 2024/121/cmpm-121-demo-2'
