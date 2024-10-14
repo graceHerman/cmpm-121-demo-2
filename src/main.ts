0bcc5d437829f5a34a1caafe5c7f6435e44606b9
graceHerman <151581400+graceHerman@users.noreply.github.com>
2024-10-14 12:42:20 -0700

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import "./style.css";
 
-const APP_NAME = "Hello DENO";
+const APP_NAME = "DENO draws";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// My edits
+// Make an app title
+const title = document.createElement('h1');
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+// Make a canvas
+const canvas = document.createElement('canvas');
+canvas.width = 256;
+canvas.height = 256;
+
+app.appendChild(canvas);
 
 // File Path: '/Users/gracelilanhermangmail.com/Desktop/Fall 2024/121/cmpm-121-demo-2'
